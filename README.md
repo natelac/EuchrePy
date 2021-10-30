69a8953db15f661e222a351ce39cf60678740c96
natelac <44073553+natelac@users.noreply.github.com>
2021-10-29 23:45:47 -0400

    Relate TODO

--- README.md
+++ README.md
@@ -1,6 +1,19 @@
 # EuchrePy: Euchre Game With Modifiable Player Interface
 
 ## TODO:
-
-- Test
-- TEST
+### Short Term
+- Remove unnecessary functions from every class.
+- Finish up scoring for whoever takes majority tricks
+  - Possibly finish up scoring for everything
+- First to 10 declared getWinner
+- Going Alone
+- Clean up code,
+- organize methods,
+- create doc strings
+- Make Euchre() class as driver instead
+### Long Term
+- APIPlayer for interfacing with a webapp through javascript
+- Create a Game Class that can be extended to create variations of euchre
+- MLPlayer that makes decisions from a trained ML/ interfaces with one
+to train it? Along with a driver class that will train the network by
+running through the game?
--- driver.py
+++ driver.py
@@ -3,44 +3,6 @@ from basicai import BasicAIPlayer
 from _team import Team
 from _standardgame import StandardGame
 
-'''
-TODO:
-- Remove unnecessary functions from every class.
-- Finish up scoring for whoever takes majority tricks
-    - Possibly finish up scoring for everything
-
-- Have taker and trick saved in StandardGame (and reset where appropriate)
-  - Instead pass game object to player?
-      - You might want to eventually create a Game superclass and have
-          StandardGame extend it
-
-- Short term
-    - First to 10 declared winner
-    - Reneging
-
-- Medium Term
-    - Going alone
-    - messaging to player for if they get reneged, telling them what happens
-    AFTER they play there card (rather than globally printing)
-
-- Once fundemental game works:
-    - Clean up code,
-    - organize methods,
-    - create doc strings
-    - DisplayAscii() function for Card() to be used in consoleplayer
-        - Colored emojis for suits and ranks
-    - Make Euchre() class as driver instead
-
-- Long Term
-    - APIPlayer for interfacing with a webapp through javascript
-    - Create a Game Class that can be extended to create variations of euchre
-    - MLPlayer that makes decisions from a trained ML/ interfaces with one
-    to train it? Along with a driver class that will train the network by
-    running through the game?
-'''
-
-
-
 p1 = HumanPlayer('User')
 ai = []
 for i in range(3):
