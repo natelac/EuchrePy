05bdf4c2f017747250b81a712d8162b7a5725e63
natelac <natelac@umich.edu>
2022-09-30 18:04:05 -0400

    Change players import

--- euchre/driver.py
+++ euchre/driver.py
@@ -1,9 +1,9 @@
 import euchre
 
-p1 = euchre.ConsolePlayer('User')
+p1 = euchre.players.ConsolePlayer('User')
 ai = []
 for i in range(3):
-    ai.append(euchre.BasicAIPlayer('AI' + str(i)))
+    ai.append(euchre.players.BasicAIPlayer('AI' + str(i)))
 team1 = euchre.Team(p1, ai[0])
 team2 = euchre.Team(ai[1], ai[2])
 game = euchre.StandardGame(team1, team2)
