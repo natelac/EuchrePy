4d728f9c0f65ae9f6e45e0fc0d8d3b102ab5f3e3
natelac <natelac@umich.edu>
2022-09-30 17:17:20 -0400

    Fix driver

--- euchre/driver.py
+++ euchre/driver.py
@@ -1,10 +1,10 @@
 import euchre
 
-p1 = euchre.ConsolePlayer('User')
+p1 = euchre.players.ConsolePlayer('User')
 ai = []
 for i in range(3):
-    ai.append(euchre.BasicAIPlayer('AI' + str(i)))
-team1 = euchre.Team(p1, ai[0])
-team2 = euchre.Team(ai[1], ai[2])
-game = euchre.StandardGame(team1, team2)
+    ai.append(euchre.players.BasicAIPlayer('AI' + str(i)))
+team1 = euchre.players.Team(p1, ai[0])
+team2 = euchre.players.Team(ai[1], ai[2])
+game = euchre.games.StandardGame(team1, team2)
 game.play()
