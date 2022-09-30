91627690a015f3640b142cd3b18eca69309be294
natelac <natelac@umich.edu>
2022-09-30 18:01:39 -0400

    Change imports

--- euchre/__init__.py
+++ euchre/__init__.py
@@ -5,7 +5,12 @@ from euchre import games
 from euchre import players
 from euchre import clients
 from euchre import server
+from euchre.games import StandardGame
 from euchre.players import Player
+from euchre.players import Team
+from euchre.players import BasicAIPlayer
+from euchre.players import ConsolePlayer
+from euchre.players import WebPlayer
 from euchre.cards import Card
 from euchre.cards import Deck
 from euchre.play import play
